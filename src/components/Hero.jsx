e9181c490ed9c46464fd717cff4ed8f6550703fd
Saher15 <saher.saadi.99@gmail.com>
2024-05-15 22:24:47 +0300

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
         <h1 className="text-6xl font-bold max-md:text-4xl text-accent-content">Best Clothing Shop Of The Year!</h1>
         <p className="py-6 text-2xl max-md:text-lg text-accent-content">
         </p>
-        <Link to="/shop" className="btn btn-wide bg-blue-600 hover:bg-blue-500 text-white">Shop Now</Link>
+        <Link to="/shop?price=10000&stock=true" className="btn btn-wide bg-blue-600 hover:bg-blue-500 text-white">Shop Now</Link>
       </div>
     </div>
   </div>
