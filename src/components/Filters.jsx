64697367f591a621b07c9ca3e12fd03f2dcdf99a
Saher15 <saher.saadi.99@gmail.com>
2024-05-15 13:55:06 +0300

    update

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -29,15 +29,7 @@ const Filters = () => {
 
   return (
     <Form className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4  gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
-      {/* SEARCH */}
-      {/* <FormInput
-        type="search"
-        label="search product"
-        name="search"
-        size="input-sm"
-        defaultValue=""
-      /> */}
-      {/* CATEGORIES */}
+      
       <FormSelect
         label="select category"
         name="category"
